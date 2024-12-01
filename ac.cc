12a0b0c45055aa00a9d4e60f2f3282ad4ceca504
John Plevyak <jplevyak@gmail.com>
2024-11-30 21:47:19 -0800

    Interim
    Signed-off-by: John Plevyak <jplevyak@gmail.com>

---
+++ ac.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string line;
+  ifstring ifs("1");
+
+  while (getline(ifs1, line)) {
+    cout << line << endl;
+  }
+  return 0;
+}
