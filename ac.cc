bcc7311d8e1eaa657c998a6d5db36de9ef766d9e
John Plevyak <jplevyak@gmail.com>
2024-12-02 09:19:16 -0800

    Interim
    Signed-off-by: John Plevyak <jplevyak@gmail.com>

--- ac.cc
+++ ac.cc
@@ -1,15 +1,45 @@
 #include <iostream>
 #include <fstream>
+#include <cmath>
 #include <string>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
 int main() {
-  string line;
-  ifstring ifs("1");
+  {
+    ifstream ifs("1");
+    std::vector<int> X;
+    std::vector<int> Y;
+    string line;
+    while (getline(ifs, line)) {
+      int x, y;
+      sscanf(line.c_str(), "%d %d", &x, &y);
+      X.push_back(x);
+      Y.push_back(y);
+    }
+    sort(X.begin(), X.end());
+    sort(Y.begin(), Y.end());
+    int t = 0;
+    for (int i = 0; i < X.size(); i++) {
+      t += abs(X[i] - Y[i]);
+    }
+    printf("1a %d\n", t);
 
-  while (getline(ifs1, line)) {
-    cout << line << endl;
+    int x = 0, y = 0;
+    t = 0;
+    while (x < X.size() && y < Y.size()) {
+      if (X[x] == Y[y]) {
+        t += Y[y];
+        y++;
+      } else if (X[x] < Y[y]) {
+        x++;
+      } else {
+        y++;
+      }
+    }
+    printf("1b %d\n", t);
   }
   return 0;
 }
--- ac.py
+++ ac.py
@@ -24,3 +24,35 @@ while i < len(X) and j < len(Y):
     else:
         j += 1
 print('1b', t)
+
+t = 0
+for l in open("2", "r").readlines():
+    x = l.strip().split()
+    x = list(map(int, x))
+    d = 0
+    for i in range(0, len(x)-1):
+       dd = x[i] - x[i+1]
+       if abs(dd) > 3 or dd == 0 or d * dd < 0:
+           break
+       d = dd
+       if i == len(x) - 2:
+           t += 1
+print('2a', t)
+
+t = 0
+for l in open("2", "r").readlines():
+    x = l.strip().split()
+    x = list(map(int, x))
+    d = 0
+    b = 0
+    for i in range(0, len(x)-1):
+       dd = x[i] - x[i+1]
+       if abs(dd) > 3 or dd == 0 or d * dd < 0:
+           if b > 0:
+               break
+           b += 1
+       else:
+           d = dd
+       if i == len(x) - 2:
+           t += 1
+print('2b', t)
